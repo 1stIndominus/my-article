23fea6c77e14128c33a3be583579e7bfb782ebeb
Nick_Radchuk <mukola.radchuk@gmail.com>
2023-01-12 22:15:04 +0200

    creating article app

---
+++ src/components/HighLight/HighLight.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import './HighLight.scss';
+
+type Props = {
+  string: string | any;
+  filter: string | any;
+}
+
+export const Highlight: React.FC<Props> = ({ string, filter }) => {
+  if (!filter) return string;
+
+  const regexp = new RegExp(filter, 'ig');
+  const matchValue = string?.match(regexp);
+
+  if (matchValue) {
+    return string?.split(regexp).map((
+      item: string,
+      index: number,
+      array: string[]
+    ) => {
+      if (index < array.length - 1) {
+        const start = matchValue.shift();
+
+        return (
+          <>
+            {item}
+            <span className="hightlight">
+              {start}
+            </span>
+          </>
+        );
+      }
+
+      return item;
+    });
+  }
+
+  return string;
+};
